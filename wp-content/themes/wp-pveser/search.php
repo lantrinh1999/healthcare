e7b209a6e32c2886cfbb5b50cb9133e41bde3ffc
Linh Tran <linhtd.contact@gmail.com>
2020-01-20 10:27:11 +0700

    code

--- wp-content/themes/wp-pveser/search.php
+++ wp-content/themes/wp-pveser/search.php
@@ -1,4 +1,5 @@
 <?php
 get_header();
+get_sidebar('home');
 ?>
 <?php get_footer(); ?>
--- wp-content/themes/wp-pveser/single.php
+++ wp-content/themes/wp-pveser/single.php
@@ -65,72 +65,13 @@ $categories=get_categories(
 						</div>
 					</div> -->
 					<div id="comments" class="comments-area mt_50">
-						<h3 class="comment-title">3 comments</h3>
-						<ul class="comment-list mt_30">
-							<li class="comment">
-								<hr>
-								<article class="comment-body mtb_30">
-									<div class="comment-avatar"> <img alt="" src="images/user2.jpg"> </div>
-									<div class="comment-main">
-										<h5 class="author-name"> <a href="#" class="comment-name">Radley Lobortis</a> <small class="comment-date">8 months ago</small> </h5>
-										<div class="comment-reply pull-right"> <a href="#"><i class="fa fa-reply" aria-hidden="true"></i> Reply</a> </div>
-										<div class="comment-content mt_10">Sed lobortis rpi luctus. Aenean posuere nulla in turluctus. Aenean posuere nulla in turs porttitor larpis porttitor larpis porttitor lauctus. Aenean posuere nulla in turpis porttitor laoreet. Quisque finibus aliquet purus.</div>
-									</div>
-								</article>
-								<hr>
-								<ol class="children">
-									<li class="comment">
-										<article class="comment-body mtb_30">
-											<div class="comment-avatar"> <img alt="" src="images/user3.jpg"> </div>
-											<div class="comment-main">
-												<h5 class="author-name"> <a href="#" class="comment-name">Lobortis Radley</a> <small class="comment-date">1 months ago</small> </h5>
-												<div class="comment-reply pull-right"> <a href="#"><i class="fa fa-reply" aria-hidden="true"></i> Reply</a> </div>
-												<div class="comment-content mt_10">Dcenas euismod faucibus dolor a finibus.Maecenas euismod faucibus dolor a finibus.Maecenas euismod faucibus dolor a finibus.Maecenas euismod faucibus dolor a finibus.cenas euismod faucibus dolor a finibus.</div>
-											</div>
-										</article>
-									</li>
-								</ol>
-							</li>
-							<li class="comment">
-								<hr>
-								<article class="comment-body mtb_30">
-									<div class="comment-avatar"> <img alt="" src="images/user1.jpg"> </div>
-									<div class="comment-main">
-										<h5 class="author-name"> <a href="#" class="comment-name">Sradle Vivamus </a> <small class="comment-date">8 days ago</small> </h5>
-										<div class="comment-reply pull-right"> <a href="#"><i class="fa fa-reply" aria-hidden="true"></i> Reply</a> </div>
-										<div class="comment-content mt_10">Vivamus imperdiet ex sed lobortis luctus. Aenean posuere nulla in turpis porttitor laoreet. Quisque finibus aliquet purus. Ut et mi eu ante interdum dignissim pellentesque a mi.</div>
-									</div>
-								</article>
-							</li>
-						</ul>
-						<div class="leave-form">
-							<h3 class="comment-title mt_50 mb_30" id="reply-title">Leave A Comment</h3>
-							<!-- Comment Form -->
-							<div class="form-style" id="contact_form">
-								<div id="contact_results"></div>
-								<div class="row">
-									<form id="contact_body" method="post">
-										<div class="col-sm-6">
-											<input class="full-with-form" type="text" name="name" placeholder="Name" data-required="true" />
-										</div>
-										<div class="col-sm-6">
-											<input class="full-with-form" type="email" name="email" placeholder="Email Address" data-required="true" />
-										</div>
-										<div class="col-sm-12 mt_30">
-											<textarea class="full-with-form" name="message" placeholder="Message" data-required="true"></textarea>
-										</div>
-										<div class="col-sm-6">
-											<button class="btn mt_30" type="submit">Leave Comment</button>
-										</div>
-									</form>
-								</div>
-							</div>
-							<!-- End Comment Form -->
-						</div>
+						<div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-width="100%" data-numposts="5"></div>
 					</div>
 				</div>
 			</div>
 		</div>
+		<div id="fb-root"></div>
+		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0"></script>
 	</div>
 </div>
 </div>
